2f0d7c93ff9020379ff95d8260ec7ed8ed8438f2
Karan Murthy <karanmurthy7.applications@gmail.com>
2017-10-28 14:49:17 -0700

    Updated Eradicate_Cold.py

---
+++ Eradicate_Cold.py
@@ -0,0 +1,31 @@
+'''Eradicate_Cold.py
+Karan Murthy, CSE 415, Autumn 2017, University of Washington
+UWNetID: karan7
+Vaibhavi Rangarajan, CSE 415, Autumn 2017, University of Washington
+UWNetID: vaibhavi
+Instructor:  S. Tanimoto.
+'''
+
+# <METADATA>
+QUIET_VERSION = "0.1"
+PROBLEM_NAME = "Eradicate Cold"
+PROBLEM_VERSION = "0.1"
+PROBLEM_AUTHORS = ['K. Murthy', 'V. Rangarajan']
+PROBLEM_CREATION_DATE = "28-OCT-2017"
+PROBLEM_DESC = \
+'''This formulation of the Eraticate_Cold uses generic
+Python 3 constructs and has been tested with Python 3.6.
+This is a model that aims to eradicate cold from the world.
+'''
+# </METADATA>
+
+# <COMMON_CODE>
+
+# This class PopulationState holds metrics such as  
+# population count, count of sick people, count of healthy people,
+# count of people born at the end of every week, and count of 
+# people who die after every week.
+class PopulationState:
+    
+
+# </COMMON_CODE>
