710360d9eef169963fd8b2dc3ae12e2368241278
Karan Murthy <karanmurthy7.applications@gmail.com>
2017-10-30 16:07:50 -0700

    Modified input0.py

--- input0.py
+++ input0.py
@@ -1,13 +1,2 @@
 population_count = 100
 sick_people_count = 91
-
-
-def goal_message(s):
-    message = "\n********************************************************************\n"
-    if HAS_VIRUS_WON:
-        message += "END: Oh no! Common Cold has taken over (90% of) the world!"
-    else:
-        message = "END: With cold under control (<10%), the World is a better place to live."
-    message += "\n********************************************************************\n"
-
-    return message
